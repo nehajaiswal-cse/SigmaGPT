968d1505964af40123439fa353c129da004de6b6
Neha Jaiswal <nehajaiawal840@gmail.com>
2026-04-10 09:41:09 +0530

    fixe url

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://sigmagpt-frontend-4b94.onrender.com",
+    origin: [process.env.CLIENT_URL,'http://localhost:3000'],
     credentials:true
 }))
 
